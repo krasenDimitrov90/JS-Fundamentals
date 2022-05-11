228353524f0bcedd4f0a643b510e93715fad0c55
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-05-11 22:43:42 +0300

    New task

---
+++ 17.Arrays Advanced - Exercise/05. Sort an Array by 2 Criteria.js
@@ -0,0 +1,9 @@
+function sortedByLengthAndSymbol(arr) {
+    arr.sort((a,b) => a.length - b.length || a.localeCompare(b))
+    
+
+    console.log(arr.join('\n'));
+    
+}
+sortedByLengthAndSymbol(['alpha', 'beta', 'gamma'])
+sortedByLengthAndSymbol(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George'])
