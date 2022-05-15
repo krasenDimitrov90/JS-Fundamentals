d22214c986522f6915c6a49c0d402afed28e3edd
krasenDimitrov90 <krasendimitrov90@gmail.com>
2022-05-15 09:17:50 +0300

    Modify

--- 17.Arrays Advanced - Lab/03. First and Last K Numbers.js
+++ 17.Arrays Advanced - Lab/03. First and Last K Numbers.js
@@ -1,9 +1,8 @@
 function solve(arr) {
-  let firstNum = arr[0];
-  let lastNumPosition = arr.length - 1;
-  console.log(arr.slice(1, firstNum + 1).join(" "));
-  console.log(arr.slice(2, firstNum + 2).join(" "));
+  let arrKNums = arr.shift();
+  console.log(arr.slice(0,arrKNums).join(' '));
+  console.log(arr.slice(arr.length - arrKNums,arr.length).join(' '));
 }
-// solve([2, 7, 8, 9])
-// solve([3, 6, 7, 8, 9])
+solve([2, 7, 8, 9])
+solve([3, 6, 7, 8, 9])
 solve([2, 0, 0, 0, 0, 0]);
